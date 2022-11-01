2c5b7afad2adf873b279e15f9f1fbdd2338828e7
Tadesse Jemal <tadessejemal63@gmail.com>
2022-11-01 19:08:17 +0300

    Updated based on comment

--- name_decorator.rb
+++ name_decorator.rb
@@ -1,3 +1,5 @@
+require_relative './nameable'
+
 # Make sure that it inherits from Nameable.
 # In the constructor assign a nameable object from params to an instance variable.
 # Implement the correct_name method that returns the result of the correct_name method of the @nameable.
