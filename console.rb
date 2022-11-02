056addc5d0d5f59a734e34314f5498f25f3b9c72
Tadesse Jemal <tadessejemal63@gmail.com>
2022-11-02 18:29:00 +0300

    make rental marked attr_accessor

--- console.rb
+++ console.rb
@@ -1,7 +1,6 @@
 require_relative './person'
 require_relative './capitalize_name_decorator'
 require_relative './trim_name_decorator'
-require_relative './person'
 require_relative './book'
 require_relative './rental'
 require_relative './student'
@@ -21,11 +20,9 @@ student2 = Person.new(17, 'Mary')
 
 book1 = Book.new('Book1 Title', 'Book1 Author')
 book2 = Book.new('Book2 Title', 'Book2 Author')
-book3 = Book.new('Book3 Title', 'Book3 Author')
 
 rental1 = Rental.new(book1, student1)
-rental2 = Rental.new(book2, student1)
-rental3 = Rental.new(book3, student2)
+rental2 = Rental.new(book2, student2)
 
 puts "Total books rented by #{student1.name} = #{student1.rentals.length}"
-puts rental1.show_rental, rental2.show_rental, rental3.show_rental
+puts rental1.show_rental, rental2.show_rental
